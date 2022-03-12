aaa1bc9f1f3db83f63eaafbdee9367c8a7dc9bed
Bandi Revanth <dhanush24082014@gmail.com>
2022-03-12 18:08:03 +0530

    Create aqua_remind.py

---
+++ aqua_remind.py
@@ -0,0 +1,13 @@
+import time
+from plyer import notification
+
+if __name__ == '__main__':
+ 	while True:
+         notification.notify(
+         title = "Drink Water Now!",
+         message ="The National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
+         app_icon = "glass.ico",
+         timeout= 10
+        )
+      
+         time.sleep(60*60)
